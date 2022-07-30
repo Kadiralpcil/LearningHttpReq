69fd03e7c8de6af525f1d5676a32817521844959
BemSinav <Kadiralpcilyedek1@gmail.com>
2022-07-30 21:37:54 +0300

    Add project files.

---
+++ LearningHttpReq/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using LearningHttpReq.Fake;
+using LearningHttpReq.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningHttpReq.Controllers
+{
+    [ Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private List<User> _users = FakeData.GetUsers(20);
+        [HttpGet]
+        public List<User> Get()
+        {
+            return _users;
+        }
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            var user = _users.FirstOrDefault(u => u.Id == id);
+            return user;
+        }
+        [HttpPost]
+        public User Post([FromBody]User user)
+        {
+            _users.Add(user);
+            return user;
+        }
+        [HttpPut]
+        public User Put([FromBody] User user)
+        {
+            var editedUser = _users.FirstOrDefault(f => f.Id == user.Id);
+            editedUser.FirstName = user.FirstName;
+            editedUser.LastName = user.LastName;
+            editedUser.Address = user.Address;
+
+            return user;
+        }
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var deletedUser = _users.FirstOrDefault(f => f.Id == id);
+            _users.Remove(deletedUser);
+            
+        }
+    }
+}
---
+++ LearningHttpReq/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningHttpReq.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String FirstName{ get; set; }
+        public String LastName{ get; set; }
+        public Object Address{ get; set; }
+    }
+}
